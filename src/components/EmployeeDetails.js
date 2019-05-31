391305fd3019b98272a7569f1df6b207c13b0fc1
arpit.kanodia <arpit.kanodia@systematixindia.com>
2019-05-31 21:23:36 +0530

    Manipulated Employee Details page

--- src/components/EmployeeDetails.js
+++ src/components/EmployeeDetails.js
@@ -1,16 +1,69 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getEmployee } from '../actions/index';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 class EmployeeDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeeDetail: {}
+    }
+  }
 
+  componentWillMount() {
+    var id = this.props.match.params.id;
+    this.props.getEmployee(id, (res) => {
+
+      if (res.status === 200) {
+        this.setState({
+          employeeDetail: res.data
+        })
+      }
+    })
+  }
   render() {
+    var employeeDetail = this.state.employeeDetail;
+    const defaultUrl = 'https://www.ibts.org/wp-content/uploads/2017/08/iStock-476085198.jpg';
     return (
       <div >
-       EmployeeDetails
+        <div className="card-custom">
+          <Card>
+            <CardImg top width="10%" className="custom-image" height="100px" src={employeeDetail.profileImage !== '' ? employeeDetail.profileImage : defaultUrl} alt="profile-image" />
+            <CardBody>
+              <div className="main">
+                <div className="name" >Name: </div>
+                <CardTitle>
+                  {employeeDetail.name}
+                </CardTitle>
+              </div>
+
+              <div className="main">
+                <div className="name" >email: </div>
+                <CardTitle>
+                  {employeeDetail.email ? employeeDetail.email : 'N/A'}
+                </CardTitle>
+              </div>
+
+              <div className="main">
+                <div className="name" >Phone: </div>
+                <CardTitle>
+                  {employeeDetail.phone ? employeeDetail.phone : 'N/A'}
+                </CardTitle>
+              </div>
+
+              <Button onClick={() => this.props.history.push('/') }>Back</Button>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     );
   }
 }
 
 //  Connect with redux through connect methode
-export default EmployeeDetails;
+export default connect(null, { getEmployee })(EmployeeDetails);
 
