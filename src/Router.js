26025df67bfd580d9e788d71fe98db08cfedda9e
arpit.kanodia <arpit.kanodia@systematixindia.com>
2019-05-31 17:44:46 +0530

    Initialize Router

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+/**
+ * @name customRoutes
+ * @param {*}
+ * @description creating custom routes for navigation
+ */
+const customRoutes = (props) => {
+    return (
+        <Switch>
+
+        </Switch>
+    )
+}
+
+export default customRoutes;
