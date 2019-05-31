6292eec5e5bc9fe882ad7852c0245460a090a0fe
arpit.kanodia <arpit.kanodia@systematixindia.com>
2019-05-31 20:21:31 +0530

    Create action to get Employee by Id

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,8 +13,26 @@ export function getEmployeesList(callback) {
         request.then((data) => {
             callback(data);
         })
-        .catch(function (error) {
-            callback(error.response);
-        });
+            .catch(function (error) {
+                callback(error.response);
+            });
+    }
+}
+
+/**
+ * Register user action
+ * @param data
+ * @param callback
+ * @returns {function(*)}
+ */
+export function getEmployee(id, callback) {
+    const request = axios.get(`http://localhost:3001/employees/${id}`);
+    return (dispatch) => {
+        request.then((data) => {
+            callback(data);
+        })
+            .catch(function (error) {
+                callback(error.response);
+            });
     }
 }
